1d73bd3676b0fdc6a96dc9c751ee0454f3b9ee12
Alexander Junge <alexander.junge@gmail.com>
2021-07-23 07:09:13 +0200

    Move News API task into main DAG

--- dags/sp500.py
+++ dags/sp500.py
@@ -1,7 +1,7 @@
 import logging
 from pathlib import Path
 from time import sleep
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 
 import finviz
 import numpy as np
@@ -11,6 +11,9 @@ import pendulum
 import yfinance as yf
 from airflow.decorators import dag, task
 from airflow.models import Variable
+from airflow.utils import timezone
+from newsapi import NewsApiClient
+from newsapi.newsapi_exception import NewsAPIException
 from pendulum.date import Date
 
 logger = logging.getLogger(__name__)
@@ -23,8 +26,11 @@ default_args = {
 }
 
 
-def get_start_end_dates(exec_date: Date) -> Tuple[Date, str, Date, str]:
-    period = exec_date - exec_date.subtract(days=int(Variable.get("sp500_recent_days")))
+def get_start_end_dates(
+    exec_date: Date, minimal_period_days: int = 0
+) -> Tuple[Date, str, Date, str]:
+    subtract_days = max(minimal_period_days, int(Variable.get("sp500_recent_days")))
+    period = exec_date - exec_date.subtract(days=subtract_days)
     start_date = period.start.date()
     start_date_str = start_date.to_date_string()
     end_date = period.end.date()
@@ -181,17 +187,86 @@ def sp500_dag():
             )
             sleep(1)
 
-    # @task()
-    # def get_news_newsapi(sp500_symbols: List[str], **context):
-    #     """
-    #     #### Get news from News API
-    #     """
-    #     pass
+    @task()
+    def get_news_newsapi(sp500_symbols: List[str], **context):
+        """
+        #### Get news from News API
+        """
+        start_date, start_date_str, end_date, end_date_str = get_start_end_dates(
+            context["execution_date"], minimal_period_days=7
+        )
+        # for each symbol, fetch news for each  day and store them as parquet
+        # files locally
+        output_dir = Path(Variable.get("sp500_output_dir")) / "news" / "newsapi"
+        client = NewsApiClient(api_key=Variable.get("news_api_key"))
+        # determine index of symbol fetched last in prev. run or start from scratch
+        start_index = 0
+        last_symbol = Variable.get("sp500_newsapi_last_symbol", default_var=None)
+        if last_symbol is not None:
+            for i, s in enumerate(sp500_symbols):
+                if s == last_symbol:
+                    start_index = (i + 1) % len(sp500_symbols)
+                    break
+        for i in range(len(sp500_symbols)):
+            ix = (start_index + i) % len(sp500_symbols)
+            s = sp500_symbols[ix]
+            logger.info(f"Processing {s}")
+            try:
+                now = timezone.utcnow()
+                news_api = client.get_everything(
+                    q=s, from_param=start_date_str, to=end_date_str, language="en"
+                )
+            except NewsAPIException as e:
+                if e.get_code() == "rateLimited":
+                    return
+                else:
+                    raise e
+            news_api_list = []
+            for a in news_api["articles"]:
+                source = a["source"]["name"]
+                author = a["author"]
+                title = a["title"]
+                description = a["description"]
+                url = a["url"]
+                published = a["publishedAt"]
+                requested = now
+                news_api_list.append(
+                    (s, published, title, description, source, author, url, requested)
+                )
+            news_api_df = pd.DataFrame(
+                news_api_list,
+                columns=[
+                    "Symbol",
+                    "Datetime",
+                    "Title",
+                    "Description",
+                    "Source",
+                    "Author",
+                    "URL",
+                    "Requested_Datetime",
+                ],
+            )
+            news_api_df["Datetime"] = pd.to_datetime(news_api_df["Datetime"])
+            news_api_df.sort_values(by=["Datetime"], inplace=True, ascending=True)
+            news_api_df.reset_index(inplace=True, drop=True)
+
+            current_out_dir = output_dir / f"Symbol={s}"
+            current_out_dir.mkdir(parents=True, exist_ok=True)
+            news_api_df.to_parquet(
+                current_out_dir / f"{start_date_str}_{end_date_str}.snappy.parquet",
+                engine="pyarrow",
+                compression="snappy",
+            )
+            # store that this symbol has been processed for coming runs
+            Variable.delete("sp500_newsapi_last_symbol")
+            Variable.set("sp500_newsapi_last_symbol", s)
+            logger.info(f"Finished {s}")
+            sleep(1)
 
     ticker_symbols = get_ticker_symbols()
     get_ticker_data(ticker_symbols)
     get_news_finviz(ticker_symbols)
-    # get_news_newsapi(ticker_symbols)
+    get_news_newsapi(ticker_symbols)
 
 
 d = sp500_dag()
